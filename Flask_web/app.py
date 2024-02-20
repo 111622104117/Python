ea4a1b9826b203018fd192ba15509028a655fc98
111622104117 <yugeec100@rmkcet.ac.in>
2024-02-20 11:20:48 +0530

    3

--- Flask_web/app.py
+++ Flask_web/app.py
@@ -3,7 +3,7 @@ from werkzeug.utils import secure_filename
 import os
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'static/videos'
+app.config['UPLOAD_FOLDER'] = 'videos'
 
 @app.route('/')
 def home():
