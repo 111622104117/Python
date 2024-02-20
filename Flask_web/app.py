258037b02da5ca153268bd52254c5d2977418056
111622104117 <yugeec100@rmkcet.ac.in>
2024-02-20 11:03:21 +0530

    1

---
+++ Flask_web/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+import os
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'static/videos'
+
+@app.route('/')
+def home():
+   return render_template('home.html')
+
+@app.route('/about')
+def about():
+   return render_template('about.html')
+
+@app.route('/upload')
+def upload_file():
+   return render_template('upload.html')
+
+@app.route('/uploader', methods = ['GET', 'POST'])
+def uploader_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      filename = secure_filename(f.filename)
+      if not os.path.exists(app.config['UPLOAD_FOLDER']):
+          os.makedirs(app.config['UPLOAD_FOLDER'])
+      f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+      return 'file uploaded successfully'
+
+if __name__ == '__main__':
+   app.run(debug = True)
