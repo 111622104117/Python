dfd9e7e1b3d9be4071ef415d6cef667a75bb0e54
111622104117 <yugeec100@rmkcet.ac.in>
2024-03-05 17:25:30 +0530

    completed

--- FlaskWeb/app.py
+++ FlaskWeb/app.py
@@ -19,17 +19,17 @@ def upload():
     return render_template('upload.html')
 
 l1='green'
-l2='blue'
+l2='red'
 l3='red'
-l4='yellow'
-c1=10
-c2=20
+l4='red'
+c1=100
+c2=40
 c3=70
-c4=60
+c4=20
 
 @app.route('/display')
 def display():
-    return render_template('display.html',l1=l1,l2=l2,l3=l3,l4=l4,c1=c1,c2=c2,c3=3,c4=c4)
+    return render_template('display.html',l1=l1,l2=l2,l3=l3,l4=l4,c1=c1,c2=c2,c3=c3,c4=c4)
 
 @app.route('/about')
 def about():
